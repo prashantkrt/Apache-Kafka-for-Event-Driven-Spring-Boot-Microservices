d9dc06e4d27d92cfe0fe5921001f3c391d4098a1
prashant <prashanttiwary10@gmail.com>
2025-02-23 19:55:06 +0530

    Apache Kafka

---
+++ EmailNotificationServiceFinal/src/main/java/com/mylearning/emailnotificationservice/handler/ProductCreatedEventHandler.java
@@ -0,0 +1,24 @@
+package com.mylearning.emailnotificationservice.handler;
+
+import com.mylearning.ProductCreatedEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaHandler;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@KafkaListener(topics = {"product-created-event-topic", "topicA"})
+public class ProductCreatedEventHandler {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+
+    //@KafkaListener(topics={"product-created-event-topic","topicA"})
+    //better way do this way
+    @KafkaHandler
+    public void handle(ProductCreatedEvent productCreatedEvent) {
+        LOGGER.info("Received a new event {}", productCreatedEvent.getTitle());
+
+    }
+
+}
