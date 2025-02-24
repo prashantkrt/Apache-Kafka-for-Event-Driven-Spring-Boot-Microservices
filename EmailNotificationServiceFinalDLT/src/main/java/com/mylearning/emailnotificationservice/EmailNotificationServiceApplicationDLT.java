fcc9863d80b6c440a89107746ba66fc124dfbc0f
prashant <prashanttiwary10@gmail.com>
2025-02-24 12:34:34 +0530

    Apache Kafka DLT

---
+++ EmailNotificationServiceFinalDLT/src/main/java/com/mylearning/emailnotificationservice/EmailNotificationServiceApplicationDLT.java
@@ -0,0 +1,13 @@
+package com.mylearning.emailnotificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailNotificationServiceApplicationDLT {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailNotificationServiceApplicationDLT.class, args);
+	}
+
+}
---
+++ EmailNotificationServiceFinalDLT/src/test/java/com/mylearning/emailnotificationservice/EmailNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mylearning.emailnotificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailNotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
